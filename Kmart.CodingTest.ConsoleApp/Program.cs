957d9e95a0c5c13e7522c954d30587b7fec02331
Giang Pham <giang.phamhoang712@gmail.com>
2021-08-21 01:15:14 +1000

    First code commit

---
+++ Kmart.CodingTest.ConsoleApp/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace Kmart.CodingTest.ConsoleApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var longestIncreasingSubsequence = FindLongestIncreasingSubsequence(string.Join(' ', args));
+        }
+
+        public static string FindLongestIncreasingSubsequence(string input)
+        {
+            if (input == string.Empty)
+            {
+                return string.Empty;
+            }
+
+            var values = input.Split(' ').Select(long.Parse).ToArray();
+
+            var longest = 1;
+            var longestStartIndex = 0;
+            var currentStartIndex = 0;
+            for (var i = 1; i <= values.Length; i++)
+            {
+                if (i < values.Length && values[i] > values[i - 1])
+                {
+                    continue;
+                }
+
+                if (i - currentStartIndex > longest)
+                {
+                    longest = i - currentStartIndex;
+                    longestStartIndex = currentStartIndex;
+                }
+
+                currentStartIndex = i;
+            }
+
+            return string.Join(' ', values.Skip(longestStartIndex).Take(longest));
+        }
+    }
+}
